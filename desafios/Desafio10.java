e1da517fac77a513ad9a9b491ff207feec1d8798
Samuel Lopes <samuelventurini2017@outlook.com>
2023-04-02 17:48:37 -0300

    Criado o arquivo Desafio10.java

---
+++ desafios/Desafio10.java
@@ -0,0 +1,13 @@
+package desafios;
+/*
+ * Escreva um programa que receba um vetor de inteiros e retorno a moda. 
+ * Onde houver empate retorno o maior. Se não tiver retorne 0.
+ */
+public class Desafio10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
